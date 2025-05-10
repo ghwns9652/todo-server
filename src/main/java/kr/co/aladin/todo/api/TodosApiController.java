ec63d7b4d5be0430202e470a5b3266e4590257df
Hojun Kim <ghwns9652@gmail.com>
2025-05-10 18:34:20 +0900

    Database setup

---
+++ src/main/java/kr/co/aladin/todo/api/TodosApiController.java
@@ -0,0 +1,40 @@
+package kr.co.aladin.todo.api;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class TodosApiController {
+
+    /*@PostMapping("/todos")
+    public void updateTodoList(@RequestBody @Valid UpdateTodoListRequest request) {
+
+    }
+
+    @GetMapping("/todos")
+    public void getTodoList() {
+
+    }
+
+    @GetMapping("/todos/{id}")
+    public void getTodo(@PathVariable("id") Long id) {
+
+    }
+
+    @PutMapping("/todos/{id}")
+    public void putTodo(@PathVariable("id") Long id, @RequestBody @Valid PutTodoByIdRequest request) {
+
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public void deleteTodo(@PathVariable("id") Long id) {
+
+    }
+
+    @GetMapping("/todos/search")
+    public List<TodoDto> searchTodo(TodoSearchCondition condition) {
+
+    }*/
+}
--- src/main/java/kr/co/aladin/todo/api/UsersApiController.java
+++ src/main/java/kr/co/aladin/todo/api/UsersApiController.java
@@ -1,4 +1,39 @@
 package kr.co.aladin.todo.api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class UsersApiController {
+/*
+    @PostMapping("/users/signup")
+    public void createUser() {
+
+    }
+
+    @PostMapping("/users/login")
+    public void login() {
+
+    }
+
+    @PostMapping("/users/login")
+    public void socialLogin() {
+
+    }
+
+    @GetMapping("/users/me")
+    public void getUser() {
+
+    }
+
+    @PutMapping("/users/me")
+    public void updateUser() {
+
+    }
+
+    @DeleteMapping("/users/me")
+    public void deleteUser() {
+
+    }*/
 }
